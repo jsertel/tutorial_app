10abed213980bf519222843046111c4f08629056
Scott Ertel <jsecpa@gmail.com>
2017-07-09 13:37:42 -0500

    finish static pages

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+
+  end
+
   test "should get home" do
     get static_page_home_url
     assert_response :success
@@ -25,5 +31,14 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
 
   end
+
+  test "should get contact" do
+    get static_page_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+
+  end
+
+
 
 end
